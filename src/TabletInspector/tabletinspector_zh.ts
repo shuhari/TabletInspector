f424cebc753604e85ef75ef2e638f249a0ebc408
shuhari <shuhari@outlook.com>
2018-09-20 17:18:06 +0800

    Update translation file.

--- src/TabletInspector/tabletinspector_zh.ts
+++ src/TabletInspector/tabletinspector_zh.ts
@@ -10,6 +10,14 @@
     </message>
 </context>
 <context>
+    <name>GeneralPage</name>
+    <message>
+        <location filename="SettingsDialog.cpp" line="11"/>
+        <source>Language</source>
+        <translation type="unfinished">语言</translation>
+    </message>
+</context>
+<context>
     <name>LogList</name>
     <message>
         <location filename="LogList.cpp" line="18"/>
@@ -20,74 +28,97 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="MainWindow.cpp" line="12"/>
+        <location filename="MainWindow.cpp" line="13"/>
         <source>Tablet Inspector</source>
         <translation type="unfinished">数位板查看器</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="33"/>
+        <location filename="MainWindow.cpp" line="34"/>
         <source>E&amp;xit</source>
         <translation type="unfinished">退出(&amp;X)</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="36"/>
+        <location filename="MainWindow.cpp" line="35"/>
         <source>&amp;Status Bar</source>
         <translation type="unfinished">状态栏(&amp;S)</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="40"/>
+        <location filename="MainWindow.cpp" line="37"/>
+        <source>&amp;Settings...</source>
+        <translation type="unfinished">设置(&amp;S)...</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="39"/>
         <source>&amp;About</source>
         <translation type="unfinished">关于(&amp;A)</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="56"/>
+        <location filename="MainWindow.cpp" line="59"/>
         <source>Tablet Information</source>
         <translation type="unfinished">数位板信息</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="58"/>
+        <location filename="MainWindow.cpp" line="61"/>
         <source>Information</source>
         <translation type="unfinished">信息</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="59"/>
+        <location filename="MainWindow.cpp" line="62"/>
         <source>Data</source>
         <translation type="unfinished">数据</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="60"/>
+        <location filename="MainWindow.cpp" line="63"/>
         <source>Logs</source>
         <translation type="unfinished">日志</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="85"/>
+        <location filename="MainWindow.cpp" line="88"/>
         <source>Toolbar</source>
         <translation type="unfinished">工具栏</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="109"/>
+        <location filename="MainWindow.cpp" line="114"/>
         <source>&amp;File</source>
         <translation type="unfinished">文件(&amp;F)</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="112"/>
+        <location filename="MainWindow.cpp" line="117"/>
         <source>&amp;View</source>
         <translation type="unfinished">查看(&amp;V)</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="120"/>
+        <location filename="MainWindow.cpp" line="125"/>
+        <source>&amp;Tools</source>
+        <translation type="unfinished">工具(&amp;T)</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="128"/>
         <source>&amp;Help</source>
         <translation type="unfinished">帮助(&amp;H)</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="171"/>
+        <location filename="MainWindow.cpp" line="187"/>
         <source>Tablet connected: %1</source>
         <translation type="unfinished">数位板已连接: %1</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="186"/>
+        <location filename="MainWindow.cpp" line="202"/>
         <source>Tablet disconnected: %1</source>
         <translation type="unfinished">数位板已断开: %1</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="SettingsDialog.cpp" line="45"/>
+        <source>Settings</source>
+        <translation type="unfinished">设置</translation>
+    </message>
+    <message>
+        <location filename="SettingsDialog.cpp" line="68"/>
+        <source>General</source>
+        <translation type="unfinished">常规</translation>
     </message>
 </context>
 <context>
